28a00650c8ad3bcac95ad5f18b61aee2ea889221
dulumary <lecture@hagulu.com>
2025-03-05 20:30:09 +0900

    [김인규]  - jpa 조회 예제

--- src/main/java/com/marondal/spring/ex/jpa/StudentController.java
+++ src/main/java/com/marondal/spring/ex/jpa/StudentController.java
@@ -1,5 +1,8 @@
 package com.marondal.spring.ex.jpa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.marondal.spring.ex.jpa.domain.Student;
+import com.marondal.spring.ex.jpa.repository.StudentRepository;
 import com.marondal.spring.ex.jpa.service.StudentService;
 
 @RequestMapping("/jpa/student")
@@ -15,6 +19,12 @@ public class StudentController {
 	
 	@Autowired
 	StudentService studentService;
+	
+	// 경고!!
+	// 절대 Controller에서 Repository를 직접 사용하면 안됨!!!!
+	// 단, 예제 진행 편의를 위해 사용하는 것
+	@Autowired
+	private StudentRepository studentRepository;
 	
 	@ResponseBody
 	@GetMapping("/create")
@@ -42,6 +52,31 @@ public class StudentController {
 		// id가 3인 학생정보 삭제
 		studentService.deleteStudent(3);
 		return "삭제 성공!";
+	}
+	
+	@ResponseBody
+	@GetMapping("/find")
+	public List<Student> findStudent() {
+		
+		List<Student> studentList = null;
+		// 모든 행 조회 
+//		List<Student> studentList = studentRepository.findAll();
+//		studentList = studentRepository.findAllByOrderByIdDesc();
+//		studentList = studentRepository.findTop2ByOrderByIdDesc();
+//		studentList = studentRepository.findByName("김인규");
+		
+//		List<String> nameList = new ArrayList<>();
+//		nameList.add("김인규");
+//		nameList.add("유재석");
+//		
+//		studentList = studentRepository.findByNameIn(nameList);
+		
+//		studentList = studentRepository.findByEmailContaining("naver");
+//		studentList = studentRepository.findByIdBetweenOrderByIdDesc(2, 4);
+		studentList = studentRepository.selectByDreamJob("개발자");
+		
+		
+		return studentList;
 	}
 	
 	@ResponseBody
